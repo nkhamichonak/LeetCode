a943cb6c821f6b39442bb089eb9f98ec4d5628e8
nkhamichonak <n.khamichonak@gmail.com>
2025-04-24 11:22:34 +0200

    Reorganisation and added problem 80

---
+++ C/80_remove_duplicates_from_sorted_array_2.c
@@ -0,0 +1,13 @@
+int	removeDuplicates(int* nums, int numsSize)
+{
+	if (numsSize < 2)
+		return (numsSize);
+
+	int i = 2;
+	for (int j = 2; j < numsSize; j++)
+	{
+		 if (nums[j] != nums[i - 2])
+			nums[i++] = nums[j];
+	}
+	return (i);
+}
